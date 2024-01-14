d811eae892636e2bcf216634efb858e70a2024b0
dylan <dylankershaw@me.com>
2024-01-13 21:37:02 -0800

    increase notebox z-index

--- src/app/components/noteBox.tsx
+++ src/app/components/noteBox.tsx
@@ -26,7 +26,7 @@ export function NoteBox(props: TextboxProps) {
                     rows={5}
                     cols={33}
                     placeholder="add notes here"
-                    className="absolute text-slate-800 mt-6 opacity-85 bg-slate-200"
+                    className="absolute text-slate-800 mt-6 opacity-85 bg-slate-200 z-20"
                     value={props.notes}
                     onChange={handleInputChange}
                 />
